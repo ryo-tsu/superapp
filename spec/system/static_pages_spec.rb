81ef44be0a6bd7da0eb98254db44a5de3f90a37a
小池諒 <koikeryou@mbp.local>
2021-07-29 15:37:51 +0900

    static_pageのシステムテスト実装

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,9 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe "トップページ" do
+    context "ページ全体" do
+      before do
+        visit root_path
+      end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+      it "世田谷市場の文字列が存在することを確認" do
+        expect(page).to have_content '世田谷市場'
+      end
+
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title
+      end
+    end
+
+    describe "ヘルプページ" do
+      before do
+        visit about_path
+      end
+  
+      it "世田谷市場とは？の文字列が存在することを確認" do
+        expect(page).to have_content '世田谷市場とは？'
+      end
+  
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title('世田谷市場とは？')
+      end
+    end
+
+    describe "利用規約ページ" do
+      before do
+        visit use_of_terms_path
+      end
+  
+      it "利用規約の文字列が存在することを確認" do
+        expect(page).to have_content '利用規約'
+      end
+  
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title('利用規約')
+      end
+    end
+  end
 end
