d69337e77df58819a2ad78866685f40bcfaa05b8
小池諒 <koikeryou@mbp.local>
2021-08-01 09:59:21 +0900

    ログインページのView実装。そのシステムテスト、リクエストテスト実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get :about,         to: 'static_pages#about'
   get :use_of_terms,  to: 'static_pages#terms'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :signup,        to: 'users#new'
   resources :users
+  get     :login,         to: 'sessions#new'
+  post    :login,         to: 'sessions#create'
+  delete  :login,         to: 'sessions#destroy'
 end
