7ea275a45269e0ab9a5f0c9e66cb767f5710e7d6
小池諒 <koikeryou@mbp.local>
2021-08-07 16:09:51 +0900

    ユーザー削除機能（テストも）実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     sequence(:email) { |n| "example#{n}@example.com" }
     password { "foobar" }
     password_confirmation { "foobar" }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
