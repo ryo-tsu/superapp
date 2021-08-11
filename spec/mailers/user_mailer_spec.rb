c8728367feec7c6457a4d35b06a2424137ec5cf4
小池諒 <koikeryou@mbp.local>
2021-08-11 17:20:47 +0900

    アカウント有効化メールテスト実装(user_mailer_test.rb)

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,32 +1,52 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+  let(:user) { create(:user) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe "アカウント有効化メール" do
+    let(:mail) { UserMailer.account_activation(user) }
+    let(:mail_body_html) { mail.html_part.body.encoded }
+    let(:mail_body_text) { mail.text_part.body.encoded }
+
+    it "ユーザーあてにメールを送信すること" do
+      expect(mail.to).to eq([user.email])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "正しい送信元からメールを送信すること" do
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "正しい件名のメールを送信すること" do
+      expect(mail.subject).to eq("世田谷市場のアカウントを有効にします")
+    end
+
+    it "ユーザの名前が正しく表示されること" do
+      expect(mail_body_html).to match(user.name)
+      expect(mail_body_text).to match(user.name)
+    end
+
+    it "アクティベーショントークンが含まれること" do
+      expect(mail_body_html).to match(user.activation_token)
+      expect(mail_body_text).to match(user.activation_token)
+    end
+
+    it "ユーザーのメールアドレスが含まれること" do
+      expect(mail_body_html).to match CGI.escape(user.email)
+      expect(mail_body_text).to match CGI.escape(user.email)
     end
   end
 
-  describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+  # describe "password_reset" do
+  #   let(:mail) { UserMailer.password_reset }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
+  #   it "renders the headers" do
+  #     expect(mail.subject).to eq("Password reset")
+  #     expect(mail.to).to eq(["to@example.org"])
+  #     expect(mail.from).to eq(["from@example.com"])
+  #   end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
-
+  #   it "renders the body" do
+  #     expect(mail.body.encoded).to match("Hi")
+  #   end
+  # end
 end
