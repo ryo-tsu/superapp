acf5cfd1f764f21b4c8783469b1e13fd6507d43c
小池諒 <koikeryou@mbp.local>
2021-08-12 14:15:38 +0900

    ユーザー有効化に関するメソッドのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ before_action :correct_user,   only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "アカウント有効化のためのメールを送信しました"
       redirect_to root_url
       # log_in @user
